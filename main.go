638ff69235d1391a2b60038a3717bdd1738d84ae
Marko Tiikkaja <marko@trustly.com>
2014-10-01 17:54:55 +0200

    Support specifying the output file

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	logpkg "log"
 	"os"
 	"os/signal"
 )
 
+func printUsage() {
+	fmt.Fprintf(os.Stderr, `Usage:
+  %s [--help] [--outfile FILE]
+
+Options:
+  --help         display this help and exit
+  --outfile      write the output to FILE (default: coverage.html)
+`, os.Args[0])
+}
+
 func main() {
+	var displayHelp bool
+	var outputFile string
+	flagSet := flag.NewFlagSet("args", flag.ExitOnError)
+	flagSet.BoolVar(&displayHelp, "help", false, "")
+	flagSet.StringVar(&outputFile, "outfile", "coverage.html", "")
+	flagSet.Usage = printUsage
+	err := flagSet.Parse(os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not parse command-line arguments: %s\n", err)
+		os.Exit(1)
+	}
+
+	if displayHelp {
+		printUsage()
+		os.Exit(0)
+	}
+
 	log := logpkg.New(os.Stderr, "", logpkg.LstdFlags)
 
 	f := &Fetcher{}
@@ -20,7 +49,7 @@ func main() {
 
 	select {
 		case <-sigint:
-			log.Print("received SIGINT, writing coverage.html")
+			log.Print("received SIGINT, writing the output file")
 			/* success */
 		case err = <-fetcherNotify:
 			log.Fatalf("fetcher failed: %s", err)
@@ -35,9 +64,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("fetcher failed: %s", err)
 	}
-	file, err := os.Create("coverage.html")
+	file, err := os.Create(outputFile)
 	if err != nil {
-		log.Fatalf("could not open coverage.html: %s", err)
+		log.Fatalf("could not open output file %#v: %s", outputFile, err)
 	}
 	defer file.Close()
 	err = Annotate(file, functions)
