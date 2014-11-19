88da9bc396a48c4ef0e9a853b63c6cf7102c79e3
Marko Tiikkaja <marko@trustly.com>
2014-11-19 13:52:36 +0100

    Support filtering the list of functions based on a regexp
    This might be useful when you're testing specific functionality.

--- main.go
+++ main.go
@@ -6,17 +6,20 @@ import (
 	logpkg "log"
 	"os"
 	"os/signal"
+	"regexp"
 )
 
 func printUsage() {
 	fmt.Fprintf(os.Stderr, `Usage:
-  %s [--help] [--outfile FILE] [--hide-source-list FILE]
+  %s [--help] [--outfile FILE] [--hide-source-list FILE] [--filter FILTER]
 
 Options:
   --help                display this help and exit
   --outfile             write the output to FILE (default: coverage.html)
   --hide-source-list    reads a newline-separated list of functions from FILE
                         for which the source code should be hidden
+  --filter              only show functions whose signature match the regular
+                        expression FILTER
 `, os.Args[0])
 }
 
@@ -24,10 +27,12 @@ func main() {
 	var displayHelp bool
 	var outputFile string
 	var hideSourceListFile string
+	var funcNameFilter string
 	flagSet := flag.NewFlagSet("args", flag.ExitOnError)
 	flagSet.BoolVar(&displayHelp, "help", false, "")
 	flagSet.StringVar(&outputFile, "outfile", "coverage.html", "")
 	flagSet.StringVar(&hideSourceListFile, "hide-source-list", "", "")
+	flagSet.StringVar(&funcNameFilter, "filter", "", "")
 	flagSet.Usage = printUsage
 	err := flagSet.Parse(os.Args[1:])
 	if err != nil {
@@ -41,6 +46,14 @@ func main() {
 	}
 
 	log := logpkg.New(os.Stderr, "", logpkg.LstdFlags)
+
+	var funcNameFilterRegexp *regexp.Regexp
+	if funcNameFilter != "" {
+		funcNameFilterRegexp, err = regexp.Compile(funcNameFilter)
+		if err != nil {
+			log.Fatalf("could not compile filter: %s", err)
+		}
+	}
 
 	f := &Fetcher{}
 	fetcherNotify, err := f.Listen("")
@@ -68,6 +81,17 @@ func main() {
 	if err != nil {
 		log.Fatalf("fetcher failed: %s", err)
 	}
+
+	if funcNameFilterRegexp != nil {
+		var filteredFunctions []*Function
+		for _, fn := range functions {
+			if funcNameFilterRegexp.MatchString(fn.Signature) {
+				filteredFunctions = append(filteredFunctions, fn)
+			}
+		}
+		functions = filteredFunctions
+	}
+
 	file, err := os.Create(outputFile)
 	if err != nil {
 		log.Fatalf("could not open output file %#v: %s", outputFile, err)
